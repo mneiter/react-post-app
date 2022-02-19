6b75c22697cc501b77c3c9b3062cb833df90a8c6
React JS <react@example.com>
2022-02-19 14:34:46 +0200

    use object to save state

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PostList from './components/Post/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 import './styles/App.css';
 
 function App() {
-  const [firstposts, setFirstPosts] = useState({
-    titel: "The first post's list", 
-    posts: [
+  const [posts, setPosts] = useState(
+    [
       {id: 1, title: 'Javascript', body: 'text text text text text'},
       {id: 2, title: 'C#', body: 'text text text text text'},
       {id: 3, title: 'Java', body: 'text text text text text'},
       {id: 4, title: 'F#', body: 'text text text text text'},
       {id: 5, title: 'Payton', body: 'text text text text text'}
-  ]});
+    ]);
 
-  const [secondposts, setSecondPosts] = useState({
-    titel: "The second post's list", 
-    posts: [
-      {id: 1, title: 'Javascript', body: 'text text text text text'},
-      {id: 2, title: 'C#', body: 'text text text text text'},
-      {id: 3, title: 'Java', body: 'text text text text text'},
-      {id: 4, title: 'F#', body: 'text text text text text'},
-      {id: 5, title: 'Payton', body: 'text text text text text'}
-  ]});
+  const [post, setPost] = useState({title: '', body: ''});
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+
+    setPosts([...posts, {...post, id: posts.length + 1}]);
+    setPost({title: '', body: ''});
+  }
 
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Post title"></input>
-        <input type="text" placeHolder="Post description"></input>
-        <button type="button">Create post</button>
+        <MyInput value={post.title}
+                  onChange={e => setPost({...post, title: e.target.value})}
+                  type="text"
+                  placeholder="Post title"></MyInput>
+        <MyInput value={post.body}
+                  onChange={e => setPost({...post, body: e.target.value})}
+                  type="text"
+                  placeholder="Post description"></MyInput>
+        <MyButton onClick={addNewPost}>Create post</MyButton>
       </form>
-      <PostList posts={firstposts.posts} title={firstposts.titel} />
-      <PostList posts={secondposts.posts} title={secondposts.titel} />
+      <PostList posts={posts} title="The list of posts" />
     </div>
   );
 }
