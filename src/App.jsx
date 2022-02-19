d7c794851bfb7b950d7312f5c93206b0f780b3a7
React JS <react@example.com>
2022-02-19 12:41:00 +0200

    create a post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-/* eslint-disable react/react-in-jsx-scope */
-import CounterMethod from './components/CounterMethod';
-import CounterClass from './components/CounterClass';
+import React, { useState } from 'react';
+import PostList from './components/Post/PostList';
+
+import './styles/App.css';
 
 function App() {
+  const [firstposts, setFirstPosts] = useState({
+    titel: "The first post's list", 
+    posts: [
+      {id: 1, title: 'Javascript', body: 'text text text text text'},
+      {id: 2, title: 'C#', body: 'text text text text text'},
+      {id: 3, title: 'Java', body: 'text text text text text'},
+      {id: 4, title: 'F#', body: 'text text text text text'},
+      {id: 5, title: 'Payton', body: 'text text text text text'}
+  ]});
+
+  const [secondposts, setSecondPosts] = useState({
+    titel: "The second post's list", 
+    posts: [
+      {id: 1, title: 'Javascript', body: 'text text text text text'},
+      {id: 2, title: 'C#', body: 'text text text text text'},
+      {id: 3, title: 'Java', body: 'text text text text text'},
+      {id: 4, title: 'F#', body: 'text text text text text'},
+      {id: 5, title: 'Payton', body: 'text text text text text'}
+  ]});
+
   return (
     <div className="App">
-      <CounterMethod />
-      <CounterClass />
-      <div calssName="post">
-        <div calssName="post">
-          <strong>1. JavaScript</strong>
-          <div>
-            Javascript is a language
-          </div>
-        </div>
-      </div>
+      <form>
+        <input type="text" placeholder="Post title"></input>
+        <input type="text" placeHolder="Post description"></input>
+        <button type="button">Create post</button>
+      </form>
+      <PostList posts={firstposts.posts} title={firstposts.titel} />
+      <PostList posts={secondposts.posts} title={secondposts.titel} />
     </div>
   );
 }
