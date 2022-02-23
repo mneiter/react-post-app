88188ffc98bb2579e673a71399bfe2b80e54e4f1
merukimon <mneiter@gmail.com>
2022-02-23 18:22:36 +0200

    move a massage 'Items not found' to the 'PostList' component

--- src/components/Post/PostList.jsx
+++ src/components/Post/PostList.jsx
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 import {v4 as uuidv4} from 'uuid'
 import PostItem from './PostItem';
 
-const PostList = props => {
+const PostList = ({posts, title, removePost}) => {
+
+    if(!posts.length){
+        return(<h1 style={{textAlign: 'center'}}>Items not found</h1>);
+    }
 
     return (
         <div className='postList'>
-          <h1 style={{textAlign: 'center'}}>{props.title}</h1>
-          {props.posts.map((post, index) => <
-              PostItem remove={props.removePost} post={post} key={uuidv4()} number={index + 1} />)}
+          <h1 style={{textAlign: 'center'}}>{title}</h1>
+          {posts.map((post, index) => <
+              PostItem remove={removePost} post={post} key={uuidv4()} number={index + 1} />)}
         </div>
     )
 }
