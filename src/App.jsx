e14ccd4ccd6d00590b7bd0e798ef410dd0ea6570
merukimon <mneiter@gmail.com>
2022-02-25 11:26:09 +0200

    add a custom hook usePosts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import PostFilter from './components/Post/PostFilter';
 import PostForm from './components/Post/PostForm';
 import PostList from './components/Post/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/modal/MyModal';
+import usePosts from './hooks/usePosts';
 
 import './styles/App.css';
 
@@ -21,16 +22,7 @@ function App() {
 
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const sorteadPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  // eslint-disable-next-line max-len
-  const sortedAndSearchedPosts = useMemo(() => sorteadPosts.filter((post) => post.title.toLowerCase().includes(filter.query.toLowerCase())), [filter.query, sorteadPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
