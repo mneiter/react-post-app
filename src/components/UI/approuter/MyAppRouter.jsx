65070a9c2e8ec27006564d45fed21fdafc86f0c5
merukimon <mneiter@gmail.com>
2022-02-27 15:12:15 +0200

    building a dynamic menu

--- src/components/UI/approuter/MyAppRouter.jsx
+++ src/components/UI/approuter/MyAppRouter.jsx
@@ -5,20 +5,24 @@ import {
   Route,
 } from 'react-router-dom';
 import MyNavbar from '../navbar/MyNavbar';
-import About from '../../../pages/About';
-import Posts from '../../../pages/Posts';
-import Error from '../../../pages/Error';
-import PostDetails from '../../../pages/PostDetails';
+import { routes } from '../../../router';
 
 function MyAppRouter() {
   return (
     <BrowserRouter>
       <MyNavbar />
       <Routes>
-        <Route path="/about" element={<About />} />
-        <Route exect path="/posts" element={<Posts />} />
-        <Route exect path="/posts/:id" element={<PostDetails />} />
-        <Route path="/error" element={<Error />} />
+        {
+          routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.element}
+              exact={route.exact}
+            />
+          ))
+
+        }
         <Route
           path="*"
           element={(<div style={{ padding: '1rem' }}><h1>There&apos;s nothing here!</h1></div>)}
