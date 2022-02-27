853f8d1df9939f0b7dffaf71c7425f2a350141a3
merukimon <mneiter@gmail.com>
2022-02-27 13:02:34 +0200

    add hook useNavigate

--- src/components/Post/PostItem.jsx
+++ src/components/Post/PostItem.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import MyButton from '../UI/button/MyButton';
 
 function PostItem(props) {
+  const navigate = useNavigate();
+
+  const redirectToPost = (id) => {
+    navigate(`/posts/${id}`);
+  };
+
   return (
     <div className="post">
       <div className="post__content">
@@ -14,6 +21,9 @@ function PostItem(props) {
         <div>
           {props.post.body}
         </div>
+      </div>
+      <div className="post__btns">
+        <MyButton onClick={() => redirectToPost(props.post.id)} type="button">Open</MyButton>
       </div>
       <div className="post__btns">
         <MyButton onClick={() => props.remove(props.post)} type="button">Delete</MyButton>
